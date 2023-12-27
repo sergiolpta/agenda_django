811a8f4d4b62b30d6421ad48b488ba7fe6c55813
sergiolpta <sergiolpta@gmail.com>
2023-12-27 19:51:30 -0300

    melhorando

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Evento
+
+# código para acessar a pagina inicial direto na agenda
+# def index(request):
+#     return redirect('/agenda/')
 
 # Create your views here.
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.all()
+    dados = {'eventos': evento}
+    return render(request, 'agenda.html', dados)
